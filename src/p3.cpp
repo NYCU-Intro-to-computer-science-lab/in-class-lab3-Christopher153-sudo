da7dcb5eb7088d656a824a2a212a88505665d0e2
Christopher153-sudo <valkry567@gmail.com>
2025-11-25 14:26:23 +0800

    Update p3.cpp

--- src/p3.cpp
+++ src/p3.cpp
@@ -3,8 +3,34 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    
-    
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    if (n == 2)
+        return true;
+    if (n % 2 == 0)
+        return false;
+
+    for (int i = 3; i * i <= n; i += 2)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    for (int c = 2; c <= n / 2; c++)
+    {
+        int b = n - c;
+        if (isPrime(c) && isPrime(b))
+        {
+            cout << c << " " << b << endl;
+            return 0;
+        }
+    }
     return 0;
 }
